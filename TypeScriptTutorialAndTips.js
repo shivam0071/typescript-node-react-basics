3aa3dcddb4390192eb0b9b23f2a038d71fd1d775
shivam0071 <shivamkoundal01@gmail.com>
2020-09-27 11:57:30 +0530

    subway project

---
+++ TypeScriptTutorialAndTips.js
@@ -0,0 +1,16 @@
+//  WHATS Typescript
+// TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.
+// Any browser. Any host. Any OS. Open source.
+// TypeScript compiles to clean, simple JavaScript code which runs on any browser, in Node.js, or in any JavaScript engine that supports
+// Types enable JavaScript developers to use highly-productive development tools and practices 
+// like static checking and code refactoring when developing JavaScript applications.
+// Types are optional, and type inference allows a few type annotations to make a big difference to the static 
+// verification of your code. Types let you define interfaces between software components and gain insights into the behavior of existing JavaScript libraries.
+// DOCS 
+// https://www.typescriptlang.org/docs/handbook/basic-types.html
+// https://devdocs.io/javascript/
+// _________________________________________________________________________________
+// ************ STRINGS ******************
+// ---------------------------------------------------------------------------------
+var color = "blue";
+console.log(color);
